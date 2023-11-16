eb8d6cbd6e8b25cbf24f8f89300b3a6859b8672a
XANA-Hub <leonardo.lembo@studio.unibo.it>
2023-11-16 12:28:05 +0100

    Fix CLAMAV

--- CLAMAVUploader.py
+++ CLAMAVUploader.py
@@ -1,25 +1,24 @@
-@app.route('/upload_file', methods=['POST'])
+import clamd
+import traceback
 
-def upload_file():
+def upload_file(file):
     status = 200
     response = {}
     try:
-       
-        file = request.files['testfile']
         cd = clamd.ClamdNetworkSocket()
-        cd.__init__(host='localhost', port=3310, timeout=None)
+        cd.__init__(host='127.0.0.1', port=3310, timeout=None)
         scan_result = cd.instream(file)
         
-        if (scan_result['stream'][0] == 'OK'):
-            message = 'file has no virus)
+        if scan_result['stream'][0] == 'OK':
+            message = 'Il file non contiene virus'
             print(scan_result['stream'])
-	        file.seek(0)
-            # <write the code to save file in local or push file to remote storage>
-        elif (scan_result['stream'][0] == 'FOUND'):
-            message = 'file has virus'
+            file.seek(0)
+            # <inserire qui il codice per salvare il file in locale o inviarlo a uno storage remoto>
+        elif scan_result['stream'][0] == 'FOUND':
+            message = 'Il file contiene un virus'
             print(scan_result['stream'])
         else:
-            message = 'Error occured while processing'
+            message = 'Si è verificato un errore durante l\'elaborazione'
         response['message'] = message
     except Exception as exp:
         print(traceback.format_exc())
@@ -27,3 +26,13 @@ def upload_file():
         response['code'] = 500
         response['message'] = str(exp)
     return response, status
+
+# Esempio di utilizzo:
+if __name__ == "__main__":
+    try:
+        # Carica un file (devi sostituire questo con il modo in cui ottieni il file)
+        with open('./LICENSE', 'rb') as file_to_scan:
+            response, status = upload_file(file_to_scan)
+            print(response, status)
+    except FileNotFoundError:
+        print("File non trovato")
