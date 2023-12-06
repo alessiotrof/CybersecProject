a79c1b4e7a2935e0bc74229d1d4e46aacb259fd7
Alessio Troffei <56554383+alessiotrof@users.noreply.github.com>
2023-12-06 18:13:25 +0100

    Now filenames and foldernames are stored in files

--- fake_fs_generator/generate_fs.sh
+++ fake_fs_generator/generate_fs.sh
@@ -2,83 +2,52 @@
 
 # Parametri dello script
 user_dir="home/default_user_dir" # La cartella radice di default da cui partire
-group_dir="home/default_group_dir"
+group_dir="home/default_group_dir" # La cartella radice di default del gruppo
 populate_group_dir=false # flag che mi dice se devo popolare la directory del gruppo o meno
 max_depth=5 # La profondità massima della gerarchia
 max_files=10 # Il numero massimo di file per cartella
 max_folders=5 # Il numero massimo di sottocartelle per cartella
 
+# Nomi dei file che usa lo script
+file_filenames="file_names.txt"
+file_foldernames="folder_names.txt"
+file_paragraphs="paragraphs.txt"
+file_titles="titles.txt"
 
 
 # Funzione che genera un nome casuale di una cartella
 gen_folder_name() {
-  local words=(
-    Documenti Immagini Progetti Download Musica Video Backup Appunti Presentazioni Archivi
-    Risorse Applicazioni Foto Lavori Manuali Progetti_Personali Pubblicazioni Sviluppo_Personale
-    Finanze Personale_2023 Viaggi Tecnologia Creativita Apprendimento Lavoro Progetti_Futuri
-    Salute Fitness Ricette Design Arte Cultura Vacanze Sogni Libri Scrittura
-    Famiglia Amici Sostenibilita Innovazione Risparmio Ricerca Progetto_Web Viaggio_Spaziale
-    Programmazione Podcast Film_Serie_Storiche Obiettivi Educazione Divertimento Risoluzione_Problemi
-    Risorse_Umane Carriera Scienza Natura Esplorazione_Territoriale Innovazione_Tecnologica
-    Studio Hobby Creativita_Tecnologica Fotografia Social_Media Archivio_Documenti
-    Progetti_Aziendali Finanze_Personali Fitness_Benessere Cucina_Artistica Viaggi_Avventura 
-    Cultura_Moderna Sogni_Ambiziosi Letture_Appassionanti Famiglia_Condivisione
-    Ecologia_Innovativa Sviluppo_Personale_Avanzato Risoluzione_Problemi_Efficienti
-    Obiettivi_Professionali Podcast_Creativi Serie_TV_Scientifiche Educazione_Continua
-    Divertimento_Sano Gestione_Tempo Risorse_Umane_Efficaci Carriera_Professionale
-    Scienza_Avanzata Natura_Esplorativa Esplorazione_Spaziale Tecnologia_Sostenibile
-    Cinema Miei_Documenti Arte_Digitale Fotografia_Creativa Progetti_Sociali
-    Imparare_Programmazione Apprendimento_Linguistico Risparmio_Energia Viaggi_Culinarie
-    Sviluppo_Apps Fitness_Spirituale Sogno_Artistico Innovazione_Sociale
-    Studiare_Natura Creazioni_Manuali Progetti_Futuristici Innovazione_Ambientale
-    Carriera_Creativa Podcast_Culturali Serie_TV_Avventurose Risoluzione_Problemi_Ecologici
-    Sostenibilita_Energetica Ricette_Creative Letture_Inspiranti Scrittura_Creativa
-    Famiglia_Felice Amici_Avventurosi Viaggi_Educativi Sogno_Tecnologico
-    Programmazione_Creativa Podcast_Educativi Film_Documentari Educazione_Scientifica
-    Sviluppo_Personale_Sostenibile Risorse_Umane_Innovative Carriera_Ambiziosa
-    Scienza_Avanzata_Espansiva Esplorazione_Naturale Tecnologia_Progressiva
-  )
+
+  if [ -f "$file_foldernames" ]; then
+    readarray -t words < $file_foldernames
+  else
+    echo "ERRORE: Il file $file_foldernames non esiste."
+    exit 1
+  fi
+
   local index=$((RANDOM % ${#words[@]}))
   echo "${words[$index]}"
 }
 
 # Funzione che genera un nome casuale di un file
 gen_file_name() {
-  local prefixes=(
-    documento foto progetto canzone video backup appunti presentazione archivio relazione immagine
-    codice rapporto brano film script tesi budget idee_spunti spese_2023 nota_diario presentazione_quarterly ricette_itinerario
-    codice_sorgente progetto_website costi_viaggio playlist_mood progetto_app idee_design progetto_libro documento_finanziario
-    lista_spese_itinerario ricerca_progetto_arte articolo_blog video_tutorial video_blogging idea_apprendimento lista_creativita
-    progetto_scienza ricetta_cucina itinerario_vacanza codice_promozionale investimenti_borsa immagini_prodotto progetto_fotografia
-    archivio_familiare documentazione_progetto progetto_manifattura 
-    scrittura_narrativa presentazione_prodotto analisi_dati presentazione_finale 
-    intervista riepilogo_mensile grafico_finanziario programma_vacanze analisi_trend 
-    programmazione_web studio_fotografico lista_spesa_itinerario ricerca_arte_storia 
-    documento_fiscale foto_ricordo progetto_innovativo canzone_rilassante video_tutorial backup_importante appunti_importanti 
-    presentazione_cliente archivio_progetti relazione_finale immagine_diagramma
-    codice_sicurezza rapporto_annuale brano_inedito film_sci-fi script_avanzato tesi_sperimentale 
-    budget_mensile idee_creative spese_mensili_2023 nota_diario_personale presentazione_trimestrale 
-    ricette_squisite_itinerario codice_sorgente_avanzato progetto_web_design costi_viaggio_affari playlist_energizzante
-    progetto_app_mobile idee_interior_design progetto_romanzo documento_contabile 
-    lista_spese_vacanza ricerca_storia_arte articolo_blog_interessante video_tutorial_fotografia 
-    idea_apprendimento_personale lista_creativita_2023 progetto_scienza_esplorativa ricetta_cucina_sana 
-    itinerario_vacanza_relax codice_promozionale_investimenti immagini_prodotto_ecologico progetto_fotografia_naturale
-    archivio_familiare_foto documentazione_progetto_importante progetto_manifattura_avanzato
-    scrittura_narrativa_avanzata presentazione_prodotto_nuovo analisi_dati_mensili 
-    presentazione_finale_approfondita intervista_professionale riepilogo_mensile_finanziario 
-    grafico_finanziario_dettagliato programma_vacanze_avventuroso analisi_trend_di_mercato
-    programmazione_web_avanzata studio_fotografico_professionale 
-    lista_spesa_itinerario_viaggio ricerca_arte_storia_creativa
-  )
+  
+  if [ -f "$file_filenames" ]; then
+    readarray -t prefixes < $file_filenames
+  else
+    echo "ERRORE: Il file $file_filenames non esiste."
+    exit 1
+  fi
 
-  local suffixes=(.txt) # è possibile aggiungere anche più suffissi qui
+
+  local suffixes=(.txt) # è possibile anche aggiungere più suffissi qui per i file intermedi
   local prefix_index=$((RANDOM % ${#prefixes[@]}))
   local suffix_index=$((RANDOM % ${#suffixes[@]}))
   echo "${prefixes[$prefix_index]}${suffixes[$suffix_index]}"
 }
 
 # Funzione che genera il contenuto casuale di un file .md con più titoli
-gen_md_file_content() {
+gen_file_content() {
 
   # Genera un numero casuale tra 3 e 5 per il numero di titoli da includere
   NUM_TITLES=$((3 + RANDOM % 10))
@@ -127,7 +96,7 @@ gen_file_hierarchy() {
     
     for ((i=0; i<$num_files; i++)); do
       local file_name=$(gen_file_name) # Il nome del file da creare
-      local file_content=$(gen_md_file_content) # Il contenuto del file da creare
+      local file_content=$(gen_file_content) # Il contenuto del file da creare
       echo -e "$file_content" > "$root_dir/$file_name" # Creazione del file
 
       # Sceglie casualmente se convertire il file appena creato in .pdf, .docx oppure odt
@@ -198,12 +167,20 @@ while true; do
 done
 
 # Legge i titoli e i paragrafi da dei file esterni
-readarray -t TITLES < titles.txt
-readarray -t PARAGRAPHS < paragraphs.txt
+if [ -f "$file_titles" ]; then
+  readarray -t TITLES < $file_titles
+else
+  echo "ERRORE: Il file $file_titles non esiste."
+  exit 1
+fi
 
-#
-#  INIZIO CICLO QUI - da far iterare fino a NUMBER
-#
+if [ -f "$file_paragraphs" ]; then
+  readarray -t PARAGRAPHS < $file_paragraphs
+else
+  echo "ERRORE: Il file $file_paragraphs non esiste."
+  exit 1
+fi
+
 
 # Itera attraverso il numero di utenti specificato
 for ((user_index=1; user_index<=NUMBER; user_index++)); do
@@ -223,9 +200,7 @@ for ((user_index=1; user_index<=NUMBER; user_index++)); do
   if [ $? -eq 0 ] && [ -z "$user_search_res" ]; then
     # L'utente non esiste, lo creo
     echo "OK! L'utente $user_name non è già esistente! Lo creo!"
-    #smbldap-useradd -P -a $user_name -s ""
     smbldap-useradd -m -P -a $user_name -s ""
-    #user_dir="/home/$user_name"
   else 
     # L'utente esiste già!
     echo "ERRORE! L'utente $user_name esiste già!"
@@ -274,7 +249,7 @@ for ((user_index=1; user_index<=NUMBER; user_index++)); do
 
       else
         # Il gruppo esiste già!
-        echo "ERRORE! Il gruppo $group_name esiste già! L'utente $user_name verrà ora inserito nel gruppo $group_name"
+        echo "Il gruppo $group_name esiste già! L'utente $user_name verrà ora inserito nel gruppo $group_name"
         smbldap-groupmod -m $user_name $group_name
 
         group_dir="/home/$group_name"
@@ -328,7 +303,9 @@ for ((user_index=1; user_index<=NUMBER; user_index++)); do
     # Elimina i file .txt generati
     find "$group_dir" -type f -name "*.txt" -delete
 
+    #
     # Per settare i permessi del gruppo
+    #
     chown root:$group_name /home/$group_name
     chmod g+rwx /home/$group_name
     echo "" >> /etc/samba/smb.conf
@@ -345,7 +322,4 @@ for ((user_index=1; user_index<=NUMBER; user_index++)); do
 
 done
 
-#
-# FINE CICLO QUI
-#
 
