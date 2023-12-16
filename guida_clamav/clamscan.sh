2433c7f0ebc911a27a504805578b70eecaaa5fa6
Alessio Troffei <56554383+alessiotrof@users.noreply.github.com>
2023-12-16 13:11:25 +0100

    for docker

---
+++ guida_clamav/clamscan.sh
@@ -0,0 +1,13 @@
+# Percorso di output del log
+LOG_FILE="/var/log/clamscan.log"
+# Percorso della cartella di quarantena
+QUARANTINE_DIR="/var/quarantine"
+
+# Crea la cartella di quarantena se non esiste
+mkdir -p "$QUARANTINE_DIR"
+
+# Esegui la scansione con clamscan nella cartella /home/ e sposta i file infetti in quarantena
+clamscan -r /home/ --log="$LOG_FILE" --move="$QUARANTINE_DIR"
+
+# Aggiungi un timestamp al log
+echo "Scan completed on $(date)" >> "$LOG_FILE"
